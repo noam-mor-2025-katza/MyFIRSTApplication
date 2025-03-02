b6f172ab1972af78225a5c9c564e4e5b9c596506
noam <noam1mor@gmail.com>
2025-03-02 16:28:49 +0200

    menu

--- app/src/main/java/com/NoamMor/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/NoamMor/myfirstapplication/MainActivity.java
@@ -2,10 +2,12 @@ package com.NoamMor.myfirstapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
@@ -27,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
     final int START_GAME = 1950;
     ImageView iv;
     ImageView iv1;
+    SharedPreferences sp;
+
+    Button btnSave;
+
+    EditText etFname,etLname;
+
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -128,7 +137,64 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+        btnSave=(Button)findViewById(R.id.btnSubmit);
+
+        btnSave.setOnClickListener(this);
+
+        etFname=(EditText)findViewById(R.id.etFname);
+
+        etLname=(EditText)findViewById(R.id.etLname);
+
+        sp=getSharedPreferences("details1",0);
+
+        tvDisplay=(TextView)findViewById(R.id.tvDisplay);
+
+        String strfname = sp.getString("fname",null);
+
+        String strlname=sp.getString("lname",null);
+
+        if(strlname!=null&&strfname!=null)
+
+        {
+
+            tvDisplay.setText("welcome " + strfname + " " + strlname);
+
+        }
+
+    }
+
+
+
+    @Override
+
+    public void onClick(View v) {
+
+
+
+        if(btnSave==v)
+
+        {
+
+            SharedPreferences.Editor editor=sp.edit();
+
+            editor.putString("fname",etFname.getText().toString());
+
+            editor.putString("lname",etLname.getText().toString());
+
+            editor.commit();
+
+        }
+}}
 
 
 
